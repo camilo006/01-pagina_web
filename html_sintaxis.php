c62fb22453167f2bd2c19945539338980cdcb401
user.email <cnunez@icesi.edu.co>
2016-07-22 17:26:19 -0500

    HTML+CSS-BASICO

--- html_sintaxis.php
+++ html_sintaxis.php
@@ -3,31 +3,41 @@
 <head>
 	<meta charset="utf-8">
 	<title>pestaña</title>
+	<link rel="stylesheet" type="text/css" href="css/plantilla.css">
 </head>
 <body>
 
 <header>
-	<div>
-		<img src="images/c.jpg" width="200" alt"logo">
+	<div class="logotipo">
+		<img src="images/logo.jpg" width="100px" alt"logo">
 	</div>
 	<nav>
 		<ul>
-			<li><a href="#">item#1</a></li>
-			<li><a href="#">item#2</a></li>
-			<li><a href="#">item#3</a></li>
+			<li><a href="#">Inicio</a></li>
+			<li><a href="#">Servicios</a></li>
+			<li><a href="#">Contacto</a></li>
 		</ul>
 	</nav>
 </header>
 
 <section class="wrapper">
-	<section class="main"></section>
+	<section class="main">
+	<section class="articles">
 		<article>
 		<center>
 			<h2>Titulo Principal</h2>
 
 		<p>Texto</p>
 		<div>
-		<img src="imagen">
+		<img src="imagen" alt="imagen">
+		</div>
+		</center>
+		</article>
+		<article>
+		<center>
+		<p>Texto</p>
+		<div>
+		<img src="imagen" alt="imagen">
 		</div>
 		</center>
 		</article>
@@ -35,13 +45,12 @@
 	<section>
 	<aside>
 	<center>
-		<h3>Titulo Secundario</h3>
+		<h3>Barra Lateral</h3>
 
 		<p>Texto</p>
 
-		<p>Texto</p>
 		<div>
-		<img src="imagen">
+		<img src="imagen" alt="imagen">
 		</div>
 		</center>
 	</aside>
@@ -49,6 +58,8 @@
 	<center>
 	<footer><p>Krato's-2016</p></footer>
 	</center>
+	</aside> 
+	</aside>
 
 </body>
 </html>
