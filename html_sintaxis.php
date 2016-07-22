4411e24ab275e2a68d0dcf1f09b27ec0e1a7ef2f
user.email <cnunez@icesi.edu.co>
2016-07-22 11:05:28 -0500

    HTML-SINTAXIS

---
+++ html_sintaxis.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>pestaña</title>
+</head>
+<body>
+
+<header>
+	<div>
+		<img src="images/c.jpg" width="200" alt"logo">
+	</div>
+	<nav>
+		<ul>
+			<li><a href="#">item#1</a></li>
+			<li><a href="#">item#2</a></li>
+			<li><a href="#">item#3</a></li>
+		</ul>
+	</nav>
+</header>
+
+<section class="wrapper">
+	<section class="main"></section>
+		<article>
+		<center>
+			<h2>Titulo Principal</h2>
+
+		<p>Texto</p>
+		<div>
+		<img src="imagen">
+		</div>
+		</center>
+		</article>
+	</section>
+	<section>
+	<aside>
+	<center>
+		<h3>Titulo Secundario</h3>
+
+		<p>Texto</p>
+
+		<p>Texto</p>
+		<div>
+		<img src="imagen">
+		</div>
+		</center>
+	</aside>
+	</section>
+	<center>
+	<footer><p>Krato's-2016</p></footer>
+	</center>
+
+</body>
+</html>
